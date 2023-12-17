09b6fe0ead0f41dbe938f08d3ca2676f297640cb
itsobi <obiobialo@aol.com>
2023-12-17 15:47:31 -0600

    dashboard and planner page updates

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -5,6 +5,11 @@ export default function Dashboard() {
   return (
     <>
       <Navbar />
+      <div className="flex justify-center items-center mt-40">
+        <h1 className="text-4xl font-extrabold">
+          Welcome (user's name inserted here)!
+        </h1>
+      </div>
     </>
   );
 }
