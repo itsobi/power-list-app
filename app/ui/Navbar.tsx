e33d871db17ac17574f1a2046d25392408e1f82c
itsobi <obiobialo@aol.com>
2023-12-17 15:47:01 -0600

    nav link styling

--- app/ui/Navbar.tsx
+++ app/ui/Navbar.tsx
@@ -1,10 +1,39 @@
+'use client';
+
 import { UserButton } from '@clerk/nextjs';
+import Link from 'next/link';
+import clsx from 'clsx';
+import { usePathname } from 'next/navigation';
+
+const links = [
+  {
+    name: 'Dashboard',
+    href: '/dashboard',
+  },
+  {
+    name: 'Planner',
+    href: '/planner',
+  },
+  {
+    name: 'Sports',
+    href: '/sports',
+  },
+];
 
 export default function Navbar() {
+  const pathname = usePathname();
   return (
     <div className="flex justify-center space-x-6 p-6 border-b shadow-sm relative">
-      <p>Planner</p>
-      <p>Sports</p>
+      {links.map((link) => (
+        <Link
+          href={link.href}
+          className={clsx('text-sm font-medium hover:text-blue-600', {
+            'text-blue-600': pathname === link.href,
+          })}
+        >
+          {link.name}
+        </Link>
+      ))}
 
       <div className="absolute top-6 right-6">
         <UserButton afterSignOutUrl="/login" />
