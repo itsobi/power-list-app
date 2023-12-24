c4c1f3eb028d102365b99ac19c2e50c54f172e99
itsobi <obiobialo@aol.com>
2023-12-24 15:55:09 -0600

    home page after sign in

--- app/login/[[...login]]/page.tsx
+++ app/login/[[...login]]/page.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 export default function Login() {
   return (
     <div className="flex justify-center items-center h-full mt-10">
-      <SignIn />
+      <SignIn afterSignInUrl="/home" />
     </div>
   );
 }
