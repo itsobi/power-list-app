f64e7096f20356aa282516737278c583ad34d499
itsobi <obiobialo@aol.com>
2023-12-25 21:55:52 -0600

    no longer a route

--- app/dashboard/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Dashboard() {
-  return (
-    <div className="flex justify-center items-center mt-40">
-      <h1 className="font-bold text-2xl lg:font-bold lg:text-4xl">
-        Welcome (user's name inserted here)!
-      </h1>
-    </div>
-  );
-}
