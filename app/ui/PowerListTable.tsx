75a878ee4d5ee79edddb9c3376b200c493bb385f
itsobi <obiobialo@aol.com>
2024-01-16 22:09:36 -0600

    handle completed mutation in table

--- app/ui/PowerListTable.tsx
+++ app/ui/PowerListTable.tsx
@@ -1,11 +1,12 @@
 import styles from '../syles/table.module.css';
 import { collectionDate } from '@/helpers/dateHelpers';
-import { deleteDoc, doc } from 'firebase/firestore';
+import { deleteDoc, doc, updateDoc } from 'firebase/firestore';
 import { Button } from '@radix-ui/themes';
 import Container from './Container';
 import { Dispatch, SetStateAction, useEffect, useState } from 'react';
 import { useUser } from '@clerk/nextjs';
 import { db } from '@/firebase/config';
+import { getTasks } from '@/helpers/taskHelpers';
 
 export default function PowerListTable({
   tasks,
@@ -28,6 +29,24 @@ export default function PowerListTable({
     setTasks(tasksCopy);
   };
 
+  // TODO: handle spamming checkbox when function is running.
+  const handleCheckbox = async (task: any, taskId: any, value: boolean) => {
+    if (!user) {
+      alert('unable to update this task.');
+      return;
+    }
+    try {
+      await updateDoc(doc(db, 'tasks', user.id, collectionDate(), taskId), {
+        isCompleted: value,
+      });
+
+      getTasks(user, setTasks);
+    } catch (error) {
+      console.error(error);
+    } finally {
+    }
+  };
+
   if (tasks.length) {
     return (
       <table className="w-full border-collapse">
@@ -42,27 +61,35 @@ export default function PowerListTable({
           </tr>
         </thead>
         <tbody>
-          {tasks.map((task, index) => (
-            <tr
-              key={task.id}
-              className={
-                index % 2 === 0 ? `${styles.evenRow}` : `${styles.row}`
-              }
-            >
-              <td className={styles.td}>{index + 1}</td>
-              <td className={`${styles.td} ${styles.maxWidthCell}`}>
-                {task.task}
-              </td>
-              <td className={styles.td}>
-                <input type="checkbox" value={task.isCompleted} />
-              </td>
-              <td className={styles.td}>
-                <Button color="red" onClick={() => handleDelete(task.id)}>
-                  Delete
-                </Button>
-              </td>
-            </tr>
-          ))}
+          {tasks.map((task, index) => {
+            return (
+              <tr
+                key={task.id}
+                className={
+                  index % 2 === 0 ? `${styles.evenRow}` : `${styles.row}`
+                }
+              >
+                <td className={styles.td}>{index + 1}</td>
+                <td className={`${styles.td} ${styles.maxWidthCell}`}>
+                  {task.task}
+                </td>
+                <td className={styles.td}>
+                  <input
+                    type="checkbox"
+                    checked={task.isCompleted}
+                    onChange={(e) =>
+                      handleCheckbox(task, task.id, e.target.checked)
+                    }
+                  />
+                </td>
+                <td className={styles.td}>
+                  <Button color="red" onClick={() => handleDelete(task.id)}>
+                    Delete
+                  </Button>
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     );
