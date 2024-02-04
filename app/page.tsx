4b9d3155325f13fb1d4030505d9b778361d14191
itsobi <obiobialo@aol.com>
2024-02-04 16:18:46 -0600

    Welcome screen text

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
+'use client';
+
+import { useUser } from '@clerk/nextjs';
+
 export default function Home() {
+  const { user } = useUser();
   return (
     <div className="flex justify-center items-center mt-20">
       <h1 className="font-bold text-2xl lg:font-bold lg:text-4xl">
-        Welcome (user's name inserted here)!
+        {user ? `Welcome ${user.firstName}!` : `Welcome!`}
       </h1>
     </div>
   );
