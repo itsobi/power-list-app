f0c6e8f03af5a1e776a67c8c19992234aefe053d
itsobi <obiobialo@aol.com>
2024-02-19 14:02:30 -0600

    fix es-lint no-unescaped -entities error

--- app/power-list/info/page.tsx
+++ app/power-list/info/page.tsx
@@ -5,18 +5,19 @@ export default function InfoPage() {
   return (
     <Container>
       <p className="justify-center">
-        Created by Andy Frisella, The "Power List" is a list of 3-5 critical
-        tasks you must complete each day that will help you make progress
-        towards your goals in life. At the end of the day, if you have completed
-        each task, mark that day with a big{' '}
+        Created by Andy Frisella, The &quot;Power List&quot; is a list of 3-5
+        critical tasks you must complete each day that will help you make
+        progress towards your goals in life. At the end of the day, if you have
+        completed each task, mark that day with a big{' '}
         <span className="text-green-500 font-bold">W</span>, if not an{' '}
         <span className="text-red-500 font-bold">L</span>. Personally, this has
         helped me tremendously in focusing on learning new technologies
         incrementally, step by step, and not getting ahead of myself. We all
         know how much information there is in the software development world,
-        and if you don't take actionable steps to progress yourself, you will
-        become stagnant. <span className="font-bold">Now is the time.</span>{' '}
-        <span className="font-bold">No more excuses</span>. Iv'e handled
+        and if you don&apos;t take actionable steps to progress yourself, you
+        will become stagnant.{' '}
+        <span className="font-bold">Now is the time.</span>
+        <span className="font-bold">No more excuses</span>. Iv&apos;e handled
         everything for you!
       </p>
       <div className="flex justify-center mt-4">
@@ -31,7 +32,7 @@ export default function InfoPage() {
         rel="noopener noreferrer"
       >
         <p className="text-center mt-2 font-extralight text-sm hover:text-slate-600 hover:underline">
-          Read more about the "Power List"
+          Read more about The &quot;Power List&quot;
         </p>
       </a>{' '}
     </Container>
